baaa2c4ca53f0bf6eff3bb13db2b2a9824ce8d34
Musbah <musbahsinno@hotmail.com>
2016-11-14 23:12:10 +0200

    The homepage now gets news from the site.

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,11 +3,37 @@ const Util = require('../../js/utilities.js');
 
 WinJS.UI.Pages.define("pages/home/home.html",
 {
-    // This function is called whenever a user navigates to this page. It
-    // populates the page elements with the app's data.
     ready: function (element, options)
     {
         document.getElementById("noRandom").style.display = "none";
         Util.showBackButton();
+
+        document.getElementById("animeLoading").style.display = "block";
+        document.getElementById("animeLoading").innerText = "Getting the latest news";
+        WinJS.xhr(
+        {
+                url: "http://myanimetracker.com/downloads/news.html"
+        })
+        .done(complete,error);
     }
 });
+
+function complete(result)
+{
+    if (result.status === 200)
+    {
+        document.getElementById("animeLoading").style.display = "none";
+        document.getElementById("newsContent").innerHTML = result.responseText;
+    }
+    else
+    {
+        document.getElementById("animeLoading").innerText = "Error getting the latest news";
+        console.log(result.status);
+    }
+}
+
+function error(err)
+{
+    document.getElementById("animeLoading").innerText = "Error getting the latest news";
+    console.log(err);
+}
